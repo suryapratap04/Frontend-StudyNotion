741e80eaed5f4dc6f28606a776fe6a02afff6a6b
Surya Pratap Singh <suryapratapsingh.cse26@jecrc.ac.in>
2024-10-19 18:54:16 +0530

    First Commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import loginImg from "../assets/Images/login.webp";
+import Template from "../Components/core/Auth/Template";
+import { useState } from "react";
+
+function Login() {
+  return (
+    <>
+      <Template
+        title="Welcome Back"
+        description1="Build skills for today, tomorrow, and beyond."
+        description2="Education to future-proof your career."
+        image={loginImg}
+        formType="login"
+      />
+    </>
+  );
+}
+
+export default Login;
